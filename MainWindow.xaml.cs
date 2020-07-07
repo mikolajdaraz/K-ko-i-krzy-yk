449dc111f23e3cb9590f66bc579d915f74d2390f
Mikolaj <mikolajdara@gmail.com>
2020-07-07 23:31:19 +0200

    Nowa klasa

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kółko_i_krzyżyk
 {
@@ -20,9 +10,65 @@ namespace Kółko_i_krzyżyk
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+        /// <summary>
+        /// Przechowuje obecna wartość w komórce podczas gry
+        /// </summary>
+        private MarkType[] mResults;
+        /// <summary>
+        /// Prawda jeśli to kolej gracza nr 1 (X) lub gracza nr 2 (0)
+        /// </summary>
+        private bool mPlayer1Turn;
+        /// <summary>
+        /// Prawda jeśli gra zakończyła się
+        /// </summary>
+        private bool mGameEndend;
+
+        #endregion
+        #region Constructor 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
+
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+        #endregion
+        /// <summary>
+        /// Rozpoczyna nową grę
+        /// </summary>
+        private void NewGame()
+        {
+            // Tworzy nową pustą tablice pustych komórek
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+            //Gracz 1 rozpoczyna grę
+            mPlayer1Turn = true;
+
+            // Dodaje każdy element do planszy
+            Cointainer.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            mGameEndend = false;
+        }
+        /// <summary>
+        /// Kliknięcie przycisku
+        /// </summary>
+        /// <param name="sender">Przyscik którey został kliknięty</param>
+        /// <param name="e">Wydarzenia po kliknięciu</param>
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ MarkType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kółko_i_krzyżyk
+{
+    /// <summary>
+    /// Rodzaj wartości w komórce obecnie to
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// Komórka jeszcze nie została kliknięta
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Komórka to 0
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// Komórka to X
+        /// </summary>
+        Cross
+    }
+}
